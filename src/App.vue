f2818bf2943d36bc1c3fdea11adcde3563eaf8d5
Nobime <romeo.nobime@mailo.com>
2022-02-01 12:16:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Home />
+</template>
+
+<script>
+import Home from './pages/Home.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './sass/main.scss';
+</style>
