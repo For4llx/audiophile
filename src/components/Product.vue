55450e6f1a10d0f9ee397ba6fcd9aaea80052e9f
Nobime <romeo.nobime@mailo.com>
2022-02-01 12:20:28 +0100

    product style home completed

---
+++ src/components/Product.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="product">
+    <h1 class="product__title">{{ title }}</h1>
+    <p v-if="isNew" class="overline product__new">New product</p>
+    <p class="product__description">{{ description }}</p>
+    <button class="button subtitle product__button">See product</button>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    isNew: Boolean,
+    title: String,
+    description: String,
+  },
+};
+</script>
