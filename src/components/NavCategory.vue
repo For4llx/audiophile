12926dfb9d12b98faf789158b621b01dcfe2b798
Nobime <romeo.nobime@mailo.com>
2022-02-01 12:21:48 +0100

    completed NavCategory style

---
+++ src/components/NavCategory.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navCategory">
+    <ul class="navCategory__menu">
+      <li class="navCategory__item">
+        <a class="navCategory__link navCategory__link--headphones">
+          <span class="navCategory__title">Headphones</span>
+          <span class="subtitle navCategory__button">Shop</span>
+        </a>
+      </li>
+      <li class="navCategory__item">
+        <a class="navCategory__link navCategory__link--speakers">
+          <span class="navCategory__title">Speakers</span>
+          <span class="subtitle navCategory__button">Shop</span>
+        </a>
+      </li>
+      <li class="navCategory__item">
+        <a class="navCategory__link navCategory__link--earphones">
+          <span class="navCategory__title">Earphones</span>
+          <span class="subtitle navCategory__button">Shop</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavCategory',
+};
+</script>
