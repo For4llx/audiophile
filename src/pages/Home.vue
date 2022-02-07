ec4b714f1806537e9d2dda1b8cd71f583d3bf69f
Nobime <romeo.nobime@mailo.com>
2022-02-07 11:57:05 +0100

    reset

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,33 +1,10 @@
 <template>
-  <header class="header">
-    <div class="wrapper wrapper--header header__wrapper">
-      <NavBar />
-      <Product
-        isNew
-        title="XX99 Mark II Headphones"
-        description="Experience natural, lifelike audio and excep
-        tional build quality made for the passionate music enthusiast."
-      />
-    </div>
-  </header>
-  <main class="home">
-    <div class="wrapper home__wrapper">
-      <NavCategory />
-    </div>
-  </main>
+  <div></div>
 </template>
 
 <script>
-import NavBar from '../components/NavBar.vue';
-import Product from '../components/Product.vue';
-import NavCategory from '../components/NavCategory.vue';
-
 export default {
   name: 'App',
-  components: {
-    NavBar,
-    Product,
-    NavCategory,
-  },
+  components: {},
 };
 </script>
