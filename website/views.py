5eafeac948bae64357988a9c4225d1cf39e57a20
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-02-22 11:36:49 +0100

    added index view

--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    """Display Home page"""
+
+    template_name = "website/index.html"
