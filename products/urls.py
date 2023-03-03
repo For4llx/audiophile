93b01ab2fc2777e88ac537a9c242d92eb46ebe31
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-03-03 12:56:55 +0100

    product card added

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductIndexView
+from .views import ProductIndexView, ProductDetailView
 
 urlpatterns = [
     path("products/", ProductIndexView.as_view(), name="products"),
+    path("1/", ProductDetailView.as_view(), name="product"),
 ]
--- products/views.py
+++ products/views.py
@@ -6,3 +6,9 @@ class ProductIndexView(TemplateView):
     """Display Products"""
 
     template_name = "products/index.html"
+
+
+class ProductDetailView(TemplateView):
+    """Display Product Detail"""
+
+    template_name = "products/product.html"
