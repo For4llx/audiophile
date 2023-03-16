97cefe1560bc47057200dd2e37404ad456231db6
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-03-16 15:44:53 +0100

    added cart

---
+++ website/static/website/javascript/cart.js
@@ -0,0 +1,23 @@
+class CartButton extends HTMLElement {
+    constructor() {
+        super();
+        let is_open = false;
+        this.addEventListener('click', this.handleClick)
+    }
+    handleClick(event) {
+        if (!this.is_open) {
+            document.getElementById("cart").style.display = "flex";
+            this.is_open = true
+        }
+        else {
+            document.getElementById("cart").style.display = "none";
+            this.is_open = false;
+        }
+    }
+}
+
+window.customElements.define("cart-button", CartButton);
+
+/* 
+
+*/
