388b8ebf9f68fd020027116c68229e75a249ed5d
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-03-16 08:40:03 +0100

    checkout done

---
+++ checkout/form.py
@@ -0,0 +1,61 @@
+from django import forms
+from .models import Billing, Shipping, Payment
+
+PAYMENT_METHODS = [
+    ("emoney", "e-Money"),
+    ("cash_on_delivery", "Cash on Delivery"),
+]
+
+
+class BillingForm(forms.ModelForm):
+    class Meta:
+        model = Billing
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "input", "placeholder": "Alexei Ward"}
+            ),
+            "phone": forms.TextInput(
+                attrs={"class": "input", "placeholder": "alexei@mail.com"}
+            ),
+            "email": forms.TextInput(
+                attrs={"class": "input", "placeholder": "+1 202-555-0136"}
+            ),
+        }
+
+
+class ShippingForm(forms.ModelForm):
+    class Meta:
+        model = Shipping
+        fields = "__all__"
+        widgets = {
+            "address": forms.TextInput(
+                attrs={"class": "input", "placeholder": "1137 Williams Avenue"}
+            ),
+            "zip_code": forms.TextInput(
+                attrs={"class": "input", "placeholder": "10001"}
+            ),
+            "city": forms.TextInput(
+                attrs={"class": "input", "placeholder": "New York"}
+            ),
+            "country": forms.TextInput(
+                attrs={"class": "input", "placeholder": "United States"}
+            ),
+        }
+
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = "__all__"
+        widgets = {
+            "payment_method": forms.RadioSelect(
+                attrs={"class": "input radio"}, choices=PAYMENT_METHODS
+            ),
+            "emoney_number": forms.TextInput(
+                attrs={"class": "input", "placeholder": "238521993"}
+            ),
+            "emoney_pin": forms.TextInput(
+                attrs={"class": "input", "placeholder": "6891"}
+            ),
+        }
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+PAYMENT_METHODS = [
+    ("emoney", "e-Money"),
+    ("cash_on_delivery", "Cash on Delivery"),
+]
+
+
+class Billing(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=50)
+
+
+class Shipping(models.Model):
+    address = models.CharField(max_length=500)
+    zip_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+
+class Payment(models.Model):
+    payment_method = models.CharField(
+        choices=PAYMENT_METHODS, max_length=50, blank=False, default="emoney"
+    )
+    emoney_number = models.CharField(max_length=9)
+    emoney_pin = models.CharField(max_length=4)
