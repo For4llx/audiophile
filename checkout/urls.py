b8255ef756a102ed4654c0688f7d63a8ec0b1e16
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-03-24 19:11:05 +0100

    added checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CheckoutView
+from .views import CheckoutView, SuccessView
 
 urlpatterns = [
     path("checkout/", CheckoutView.as_view(), name="checkout"),
+    path("checkout/success", SuccessView.as_view(), name="success"),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -19,5 +19,27 @@ class CheckoutView(TemplateView):
         return context
 
     def post(self, request):
-        print("hello world")
+        return redirect("/checkout/success")
+
+
+class SuccessView(TemplateView):
+
+    template_name = "checkout/success.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        customer = Customer.objects.get(device=self.request.COOKIES.get("device"))
+        context["order"] = Order.objects.get(customer=customer, complete=False)
+        context["billing_form"] = BillingForm()
+        context["shipping_form"] = ShippingForm()
+        context["payment_form"] = PaymentForm()
+        return context
+
+    def post(self, request):
+        customer, created = Customer.objects.get_or_create(
+            device=self.request.COOKIES.get("device")
+        )
+        order = Order.objects.get(customer=customer, complete=False)
+        order.delete()
+
         return redirect("/")
